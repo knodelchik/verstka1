56371de38b7c0f0fd0db3c7a510e98d2855181d1
Bogdan Ulanovskiy <ulanovskiy1110@gmail.com>
2025-03-13 22:02:23 +0200

    commit

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-const arr = [1,2,3,4,6];
+const tabBtns = document.querySelectorAll(".tabs-nav button");
+const tabItems = document.querySelectorAll(".tabs-item");
 
-let sum = 0;
+function hideTab() {
+    tabItems.forEach(item => item.classList.add("hide"));
+    tabBtns.forEach(btn => btn.classList.remove("active"));
+};
 
-arr.forEach(function  (num){
-    sum += num;
-})
+function showItems(index) {
+    tabItems[index].classList.remove("hide");
+    tabBtns[index].classList.add("active");
+};
 
-console.log(sum);
+hideTab();
+showItems(0);
+
+tabBtns.forEach((btn, index) => btn.addEventListener("click", () => {
+    hideTab();
+    showItems(index);
+}))
+
+const anchors = document.querySelectorAll(".nav a");
+
+anchors.forEach(anc => {
+    anc.addEventListener("click", function (event) {
+        event.preventDefault();
+        
+        const id = anc.getAttribute("href");
+
+        const elem = document.querySelector(id);
+
+        window.scroll({
+            top:elem.offsetTop - 75,
+            behavior: "smooth"
+        })
+    })
+});
